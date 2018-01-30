68aa4fe1fadec2d387c08abf0d4c0552d7e344fe
Jean-Marc Robin <jmr33bx@yahoo.fr>
2018-01-30 07:30:51 +0100

    Add Bootstrap Template

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// src/App.jsx
+
+import React from 'react';
+
+class App extends React.Component {
+  render () {
+    return (
+      <div className = "container">
+        <h2 className = "app-title">Flashcard React Redux Enzyme</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
+// src/index.js
+
+/*eslint-disable no-unused-vars*/
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
